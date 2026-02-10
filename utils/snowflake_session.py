09ff627bec826207fe8a0b0cb5b1260f0af40919
Tejas  Muley <tejas.muley@udchalo.com>
2026-02-10 16:55:44 +0530

    Fixing Issues of Streamlit Connection

--- utils/snowflake_session.py
+++ utils/snowflake_session.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import snowflake.connector
+from snowflake.snowpark import Session
 from cryptography.hazmat.primitives import serialization
 
 
@@ -7,9 +7,8 @@ def get_snowflake_session():
 
     snowflake_config = st.secrets["snowflake"]
 
-    # Read private key from file
-    with open(snowflake_config["private_key_path"], "rb") as key_file:
-        private_key = key_file.read()
+    # Load private key from secrets
+    private_key = snowflake_config["private_key"].encode()
 
     p_key = serialization.load_pem_private_key(
         private_key,
@@ -22,14 +21,14 @@ def get_snowflake_session():
         encryption_algorithm=serialization.NoEncryption()
     )
 
-    conn = snowflake.connector.connect(
-        user=snowflake_config["user"],
-        account=snowflake_config["account"],
-        private_key=pkb,
-        role=snowflake_config["role"],
-        warehouse=snowflake_config["warehouse"],
-        database=snowflake_config["database"],
-        schema=snowflake_config["schema"]
-    )
+    connection_parameters = {
+        "account": snowflake_config["account"],
+        "user": snowflake_config["user"],
+        "private_key": pkb,
+        "role": snowflake_config["role"],
+        "warehouse": snowflake_config["warehouse"],
+        "database": snowflake_config["database"],
+        "schema": snowflake_config["schema"],
+    }
 
-    return conn
+    return Session.builder.configs(connection_parameters).create()
